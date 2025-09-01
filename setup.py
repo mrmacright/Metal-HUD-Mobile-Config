c217c2f0f30b4c28c852e007b9fc16cdcbe2e812
MrMacRight <83794957+mrmacright@users.noreply.github.com>
2025-09-01 12:14:33 +1000

    Fixes

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ OPTIONS = {
     "semi_standalone": False, 
     "plist": {
         "CFBundleName": "Metal HUD Mobile Config",
-        "CFBundleShortVersionString": "2.0.0",
-        "CFBundleVersion": "2.0.0",
+        "CFBundleShortVersionString": "2.0.1",
+        "CFBundleVersion": "2.0.1",
         "CFBundleIdentifier": "com.stewie.metalhud",
     },
 }
